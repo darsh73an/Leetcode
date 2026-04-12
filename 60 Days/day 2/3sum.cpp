7f212c47a72590e067937194fb4e5592b95ac63a
Darshan R <darshankok7@gmail.com>
2026-04-12 07:06:11 +0530

    day 2 two,3,4sum completed

--- 60 Days/day 2/3sum.cpp
+++ 60 Days/day 2/3sum.cpp
@@ -20,7 +20,7 @@ public:
                     ans.push_back({nums[i],nums[left],nums[right]});
 
                 while(left < right && nums[left] == nums[left+1]) left++;
-                while(left < right && nums[right] == nums[right+1]) right++;
+                while(left < right && nums[right] == nums[right-1]) right--;
 
                     left++;
                     right--; // bcoz we wnat more triplets
